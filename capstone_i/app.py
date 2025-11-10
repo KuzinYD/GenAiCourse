799d44a770d166ee3af328e9b89953d0cfcd3f7f
Yan Kuzin <iankuzin@gmail.com>
2025-11-10 15:51:02 +0500

    clean up

--- capstone_i/app.py
+++ capstone_i/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+from utils import get_logger
+
+logger = get_logger("genai_capstone_app")
 
 st.set_page_config(page_title="GenAI Capstone Project", page_icon="🤖")
 
 st.title("GenAI Capstone Project")
 st.write("Work in progress")
+
+logger.info("Streamlit app started")
---
+++ capstone_i/utils/__init__.py
@@ -0,0 +1,5 @@
+# utils package
+from .logger_helper import get_logger
+
+__all__ = ["get_logger"]
+
